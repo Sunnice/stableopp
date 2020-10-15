3e327696873fcf849faefce357a23ae51dda177f
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-10-15 19:01:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,21 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+rm -rf package/lean/luci-app-rclone
+rm -rf package/lean/rclone-ng
+rm -rf package/lean/rclone-webui-react
+rm -rf package/lean/rclone
+
+rm -rf feeds/packages/net/smartdns
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns package/net/smartdns	
+
+rm -rf feeds/packages/net/https-dns-proxy
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
+
+svn co https://github.com/Lienol/openwrt-packages/trunk/utils/syncthing package/utils/syncthing
